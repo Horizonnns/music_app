c7b77eb10797d9b69f285a79863bb60ecf29d4de
Horizonnns <for.office.gthb@gmail.com>
2024-02-02 15:56:05 +0500

    Created a track component and sent data with props

---
+++ src/components/Track/Track.jsx
@@ -0,0 +1,7 @@
+import style from './track.module.scss';
+
+const Track = (track) => {
+	return <div>{JSON.stringify(track)}</div>;
+};
+
+export default Track;
--- src/pages/main/MainPages.jsx
+++ src/pages/main/MainPages.jsx
@@ -1,12 +1,15 @@
 import tracksList from './tracksList';
 import style from './mainPage.module.scss';
+import Track from '../../components/Track/Track';
 
 const MainPages = () => {
 	return (
 		<div className={style.search}>
 			<>Поиск треков</>
 			<div className={style.list}>
-				<p>Музыкальное приложение</p>
+				{tracksList.map((track) => (
+					<Track track={track} key={track.id} />
+				))}
 			</div>
 		</div>
 	);
