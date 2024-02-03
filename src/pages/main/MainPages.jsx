da2b43b2ea039382c05e4d9bf92c7cf7fddd968e
Horizonnns <for.office.gthb@gmail.com>
2024-02-03 15:32:31 +0500

    Made a method for searching tracks and added an input to it

--- src/pages/main/MainPages.jsx
+++ src/pages/main/MainPages.jsx
@@ -1,13 +1,49 @@
+import { useState } from 'react';
 import tracksList from './tracksList';
 import style from './mainPage.module.scss';
 import Track from '../../components/Track/Track';
+import { Input } from '@mui/material';
+
+const searchedTracks = (query) => {
+	if (!query) {
+		return tracksList;
+	}
+
+	const lowerCaseQuery = query.toLowerCase();
+
+	return tracksList.filter(
+		(track) =>
+			track.title
+				.toLowerCase()
+				.includes(lowerCaseQuery) ||
+			track.artists
+				.toLowerCase()
+				.includes(lowerCaseQuery)
+	);
+};
 
 const MainPages = () => {
+	const [tracks, setTracks] =
+		useState(tracksList);
+
+	const handleChange = (event) => {
+		const foundTracks = searchedTracks(
+			event.target.value
+		);
+
+		setTracks(foundTracks);
+	};
+
 	return (
 		<div className={style.search}>
-			<>Поиск треков</>
+			<Input
+				className={style.input}
+				placeholder='Поиск треков'
+				onChange={handleChange}
+			/>
+
 			<div className={style.list}>
-				{tracksList.map((track) => (
+				{tracks.map((track) => (
 					<Track {...track} key={track.id} />
 				))}
 			</div>
